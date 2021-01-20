46f541db21051d69f07486534b90456235a5c41e
majkelmichel <51023578+majkelmichel@users.noreply.github.com>
2021-01-20 18:51:29 +0100

    Ex. 8.9 done

--- src/queries.js
+++ src/queries.js
@@ -9,3 +9,14 @@ export const ALL_AUTHORS = gql`
         }
     }
 	`;
+
+export const ALL_BOOKS = gql`
+    query {
+        allBooks {
+            title
+            author
+            published
+            id
+        }
+    }
+    `;
